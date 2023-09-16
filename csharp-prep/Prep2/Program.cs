63e5ff2c73c27a103858062bc72d6cb3cae3717e
wolffjo <justjoeking123@gmail.com>
2023-09-16 10:38:35 -0600

    Grade file without stretch challenge

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter = "";
+        Console.Write("What is you grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage <= 89 && gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage <= 79 && gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage <= 69 && gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            Console.WriteLine("HoI! i'M TeMmiE!");
+        }
+        Console.WriteLine($"Your grade is {letter}.");
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You pass the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass the class, but if you worker harder, you can!");
+        }
     }
 }
