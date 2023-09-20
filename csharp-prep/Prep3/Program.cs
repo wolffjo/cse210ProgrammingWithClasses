a8bc63d89638edc76ee561f22c48a0b72e6b545f
wolffjo <justjoeking123@gmail.com>
2023-09-20 09:54:14 -0600

    Magic Number Prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int mNumber = randomGenerator.Next(1, 101);
+        int guess = 999999999;
+
+        string input = "";
+
+        //Console.WriteLine("What is your magic number? ");
+        //string input = Console.ReadLine();
+        //int mNumber = int.Parse(input);
+
+        while(guess != mNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            input = Console.ReadLine();
+            int gNum = int.Parse(input);
+            if (gNum < mNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(gNum > mNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                guess = mNumber;
+            }
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
