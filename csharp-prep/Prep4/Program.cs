7df3ed20f58689e49d69a4860508c148a59031d9
wolffjo <justjoeking123@gmail.com>
2023-09-21 20:23:58 -0600

    List of numbers program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int num = 99999;
+        int count = 0;
+        int adding = 0;
+        int greatest = 0;
+        string input = "";
+        List<int> numbers = new List<int>();
+
+        while (num != 0)
+        {
+            Console.WriteLine("Enter numbers to add to a list. Type 0 to finish. ");
+            input = Console.ReadLine();
+            num = int.Parse(input);
+            numbers.Add(num);
+
+        }
+
+        foreach (int item in numbers)
+        {
+            Console.WriteLine(item);
+            adding += item;
+            count += 1;
+            if(greatest < item)
+            {
+                greatest = item;
+            }
+        }
+        int average = adding / count;
+        Console.WriteLine($"The sum is {adding}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {greatest}");
+
     }
 }
